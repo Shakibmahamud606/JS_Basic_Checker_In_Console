80be443313aee2a698c03b701ba61a9876492ac9
Shakibmahamud606 <shakibmahamud0908@gmail.com>
2020-10-15 23:36:29 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,67 @@
+// var s=0;
+// while (s<5){
+// 	console.log ("hello ");
+// 	s++;
+// }
+// console.log("hi world");
+
+// // for loop  
+
+
+// for (var i =0; i<5; i++) {
+// 	console.log("hi  for loop sw")
+// }
+
+// // while
+
+
+// num=0;
+// while(num<25){
+// 	if(num%2 !==0)
+// 	{
+// 		console.log(num);
+// 	}
+// 	num++;
+// }
+
+
+// // for
+
+
+// for (var i = 25; i < 50; i++) {
+// 	if (i%2 !==0)
+// 	{
+// 		console.log(i);
+// 	}
+// }
+
+       // Ask some quession??????/////
+
+
+
+       var firstName=prompt("Give the First Name : ")
+       var lastName=prompt("Give the Last Name : ")
+       var  age =prompt("Give age  : ")
+       var hight=prompt("Higth in CM : ")
+       var pateName=prompt("Enter your  pate name  : ")
+       alert("Thanks  for  your information ")
+
+
+       if (firstName[0]===lastName[0])
+       {
+       	if(age >=20 && age<=30)
+       	{
+       		if  (hight>=170)
+       		{
+       			if (pateName[pateName.length-1]==="y")
+       			{
+       				console.log("welcome spy");
+       			}
+       		}
+       	}
+       }
+       else
+       {
+       	console.log("Who are you!!!!!!!!!!!!!");
+       }
+
